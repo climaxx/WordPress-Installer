1573fa8e27e24608a7a511457b607bd8b02f3d83
Stefan Marogel <office@brunomag.ro>
2020-03-21 15:31:23 +0200

    Added possibility of selecting which WordPress version to install

--- installer.php
+++ installer.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * WordPress command-line / web-based installer 
+ * WordPress command-line / web-based installer
  * You just pass the parameters to the database and the installer will download the latest version
  * of WordPress, add these parameters and add the salt parameters
  * It automatically creates a new "wordpress" directory in the same directory as the installer
- * 
+ *
  * @package: WordPressInstaller
  * @author: Brunomag Concept SRL
  * @autor-uri: www.brunomag.ro
@@ -14,24 +14,34 @@ $typeOfPHPExecution = php_sapi_name();
 
 //check to see if it is a command line execution
 if ($typeOfPHPExecution == 'cli'){
-
-    if (count($argv) == 1)
-           die('Run the installer in the the directory in which you wish to install Wordpress. 
-         Usage: installer.php db_name db_user db_pass db_host
-         ');
-    
+    if (count($argv) != 5 && count($argv) != 6){
+        print_r($argv);
+        print_r(count($argv));
+        die("Run the installer in the the directory in which you wish to install Wordpress. For <optional_version> leave black for latest version or please enter the full official number from https://wordpress.org/download/releases/ (e.g. \"5.3.2\")
+            Usage: installer.php db_name db_user db_pass db_host <optional_version>\n
+            ");
+    }
+    if (!file_exists('/usr/local/bin/wp') && !file_exists('/usr/bin/wp')){
+        die("ERROR - missing wp - Please install the WordPress command line interface from https://wp-cli.org/\n");
+    }
 
     $hostname = addslashes($argv[4]);
     $dbname = addslashes($argv[1]);
     $dbuser = addslashes($argv[2]);
     $dbpass = addslashes($argv[3]);
+    if (isset($argv[5])){
+        $archiveFileName = "wordpress-".$argv[5].".zip";
+    }
+    else {
+        $archiveFileName = "latest.zip";
+    }
 
 }
 else {
-        $hostname = addslashes($_REQUEST['hostname']);
-        $dbname = addslashes($_REQUEST['dbname']);
-        $dbuser = addslashes($_REQUEST['dbuser']);
-        $dbpass = addslashes($_REQUEST['dbpass']);
+    $hostname = addslashes($_REQUEST['hostname']);
+    $dbname = addslashes($_REQUEST['dbname']);
+    $dbuser = addslashes($_REQUEST['dbuser']);
+    $dbpass = addslashes($_REQUEST['dbpass']);
 }
 
 //Verifying for empty values
@@ -48,9 +58,11 @@ if (trim($dbpass) == ''){
     die("Error: DB password is empty!");
 }
 
+if (!($archiveFileName)){
+    $archiveFileName = "latest.zip";
+}
 
-$zipUrl = 'http://wordpress.org/latest.zip';
-$archiveFileName = "latest.zip";
+$zipUrl = 'http://wordpress.org/' + $archiveFileName;
 
 $filePointer = fopen($archiveFileName, "w");
 $downloadedZipFileContents = file_get_contents($zipUrl);
@@ -104,31 +116,31 @@ foreach ($wpconfig as $lineNumer => $line){
     //matches 2 lines
     if (preg_match('/AUTH_KEY/', $line)){
        $line = "";
-    }   
+    }
 
     if (preg_match('/LOGGED_IN_KEY/', $line)){
        $line = "";
-    }   
+    }
 
     if (preg_match('/NONCE_KEY/', $line)){
        $line = "";
-    }   
+    }
 
     //matches 2 lines
     if (preg_match('/AUTH_SALT/', $line)){
        $line = "";
-    }   
-    
+    }
+
     if (preg_match('/LOGGED_IN_SALT/', $line)){
        $line = "";
-    }   
+    }
 
     if (preg_match('/NONCE_SALT/', $line)){
        $line = "#REPLACEME#";
-    }   
+    }
 
     $wpconfigNewFile .= $line;
-    
+
 
 }
 
